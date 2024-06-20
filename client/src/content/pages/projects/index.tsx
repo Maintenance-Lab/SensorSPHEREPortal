06acb84341c2f1b35c50a838c1b895670cd1ac25
Nedim Bjelic <nedimbjelic2003@gmail.com>
2024-06-20 12:53:02 +0200

    Prepare All Projects for pinning

--- client/src/content/pages/projects/index.tsx
+++ client/src/content/pages/projects/index.tsx
@@ -28,6 +28,7 @@ import {
   GridToolbarQuickFilter
 } from '@mui/x-data-grid';
 import CreateProjectDialog from './CreateProjectDialog';
+import { PushPin } from '@mui/icons-material';
 
 const fetchActiveProjects = async () => {
   const res = await fetch('/api/projects/active', {
@@ -139,6 +140,27 @@ function CustomProjectsToolbar({ selectedProjectIds, setSelectedProjectIds, fetc
     }
   }, [selectedProjectIds]);
 
+  const handlePinProjects = useCallback(async () => {
+    try {
+      for (const projectId of selectedProjectIds) {
+        const response = await fetch('/api/account/pin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            credentials: 'include'
+          },
+          body: JSON.stringify({ projectId })
+        });
+
+        if (!response.ok) {
+          console.error('Failed to pin project');
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [selectedProjectIds]);
+
   return (
     <GridToolbarContainer sx={{ padding: 1 }}>
       <Stack direction="row" spacing={1}>
@@ -151,6 +173,15 @@ function CustomProjectsToolbar({ selectedProjectIds, setSelectedProjectIds, fetc
           Create Project
         </Button>
         <GridToolbarQuickFilter variant="outlined" size='small' sx={{ padding: 0 }} />
+        {/* <Button
+          variant="outlined"
+          size="medium"
+          startIcon={<PushPin />}
+          disabled={!activeSelection}
+          onClick={handlePinProjects}
+        >
+          Pin
+        </Button> */}
         <Button
           variant="outlined"
           size="medium"

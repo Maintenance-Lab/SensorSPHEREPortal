220436b0775ddd5744e4dac88920565697b1de90
Nedim Bjelic <nedimbjelic2003@gmail.com>
2024-06-20 12:17:03 +0200

    Guidance on home page how to get started

--- client/src/content/pages/home/index.tsx
+++ client/src/content/pages/home/index.tsx
@@ -70,7 +70,7 @@ const Home = () => {
       <PageTitleWrapper>
         <Typography variant="h1">Home</Typography>
       </PageTitleWrapper>
-      <CreateProjectDialog open={openCreateProjectDialog} setOpen={setOpenCreateProjectDialog} /> 
+      <CreateProjectDialog open={openCreateProjectDialog} setOpen={setOpenCreateProjectDialog} />
       <Container>
         <Grid container spacing={2} >
           {loading && (
@@ -107,19 +107,21 @@ const Home = () => {
         <Stack spacing={2} mt={4}>
           <Typography variant="h2">Getting Started</Typography>
         </Stack>
-        <Stack direction="row" spacing={2} mt={2}>
-          <Card sx={{ flex: 1 }}>
-            <CardActionArea
-              sx={{ p: 2 }}
-              onClick={() => setOpenCreateProjectDialog(true)}
-            >
-              <Stack spacing={1}>
-                <DesignServices fontSize='large' />
-                <Typography variant="h6">Create a New Project</Typography>
-                <Typography variant="subtitle1">Create a new project, add devices, and start data collection sessions.</Typography>
-              </Stack>
-            </CardActionArea>
-          </Card>
+        <Grid container spacing={2} mt={1}  py={2}>
+          <Grid item xs={12} lg={4}>
+              <Typography variant="h2" color="primary.main">1</Typography>
+              <Typography variant="body1" mt={1}>Create a data collection project.</Typography>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Typography variant="h2" color="primary.main">2</Typography>
+            <Typography variant="body1" mt={1}>Find and add your devices.</Typography>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Typography variant="h2" color="primary.main">3</Typography>
+            <Typography variant="body1" mt={1}>Start a data collection session in the project.</Typography>
+          </Grid>
+        </Grid>
+        <Stack direction="row" spacing={2} mt={3}>
           <Card sx={{ flex: 1 }}>
             <CardActionArea
               sx={{ p: 2 }}
@@ -129,6 +131,18 @@ const Home = () => {
                 <Usb fontSize='large' />
                 <Typography variant="h6">Find Devices</Typography>
                 <Typography variant="subtitle1">Search and find devices, view sensors, and use them in your projects.</Typography>
+              </Stack>
+            </CardActionArea>
+          </Card>
+          <Card sx={{ flex: 1 }}>
+            <CardActionArea
+              sx={{ p: 2 }}
+              onClick={() => setOpenCreateProjectDialog(true)}
+            >
+              <Stack spacing={1}>
+                <DesignServices fontSize='large' />
+                <Typography variant="h6">Create a New Project</Typography>
+                <Typography variant="subtitle1">Create a new project, add devices, and start data collection sessions.</Typography>
               </Stack>
             </CardActionArea>
           </Card>

c1c14dbf7f051e4762cdb420fd0b980a5b293381
TOGO03 <goemanstom@gmail.com>
2024-11-11 16:29:38 +0100

    tabs active and archived sessions (+ size adjustments)
    not possible to archive and unarchive sessions via the table yet.

--- client/src/content/pages/projects/index.tsx
+++ client/src/content/pages/projects/index.tsx
@@ -257,12 +257,14 @@ const Projects = () => {
               rows={projectsRows}
               columns={projectsColumns}
               density="compact"
+              pageSizeOptions={[10, 25, 50]}
               autoHeight
               autosizeOnMount
               autosizeOptions={{ includeOutliers: true }}
               checkboxSelection={true}
               onRowSelectionModelChange={(newSelection) => setSelectedProjectIds(newSelection)}
               initialState={{
+                pagination: { paginationModel: { pageSize: 10 } },
                 columns: {
                   columnVisibilityModel: {
                     lastActive: false
